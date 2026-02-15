7ae36fd9106a811f4f5ef313f294a0cfc3fb644e
lukayanes <luka@summitgroupacq.com>
2026-02-15 10:46:07 -0500

    Add openMenu and closeMenu functions

--- script.js
+++ script.js
@@ -89,3 +89,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+function openMenu() {
+  document.getElementById("sideMenu").classList.add("active");
+  document.getElementById("menuOverlay").classList.add("active");
+}
+
+function closeMenu() {
+  document.getElementById("sideMenu").classList.remove("active");
+  document.getElementById("menuOverlay").classList.remove("active");
+}
+
